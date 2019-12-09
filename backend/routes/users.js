e8270d4eefaf5dd97c09ec4e848f1d73c18a95ae
Lauri Nuotio <lauri.nuotio@siili.com>
2019-12-09 12:47:15 +0200

    feat(jwt): init jwt usage

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,16 +2,20 @@ const auth = require('../middleware/auth')
 const bcrypt = require('bcrypt')
 const express = require('express')
 const router = express.Router()
+const config = require('config')
+const jwt = require('jsonwebtoken')
 
 router.get('/current', auth, async (req, res) => {
   const user = await User.findById(req.user._id).select('-password')
   res.send(user)
 })
 
+router.post('/register', async (req, res) => {
+  const { name, email, password } = req.body
+})
+
 router.post('/', async (req, res) => {
   // validate the request body first
-  const { error } = validate(req.body)
-  if (error) return res.status(400).send(error.details[0].message)
 
   user = {
     _id: '123',
@@ -19,8 +23,21 @@ router.post('/', async (req, res) => {
     email: 'tes@ti.fi',
   }
 
-  const token = user.generateAuthToken()
-  res.header('x-auth-token', token).send({
+  const token = jwt.sign(
+    {
+      sub: '1234567890',
+      name: 'Node App',
+      admin: true,
+      iat: 1516239022,
+      'https://hasura.io/jwt/claims': {
+        'x-hasura-allowed-roles': ['editor', 'user', 'mod'],
+        'x-hasura-default-role': 'user',
+      },
+    },
+    config.get('myprivatekey')
+  )
+
+  res.header('Authorization', 'Bearer ' + token).send({
     _id: user._id,
     name: user.name,
     email: user.email,
