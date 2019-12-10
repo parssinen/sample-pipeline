6a3a058f304b89f624fa7119208e24a89686c584
Lauri Nuotio <lauri.nuotio@siili.com>
2019-12-10 13:07:14 +0200

    feat(register/login): basic setup

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,47 +1,48 @@
 const auth = require('../middleware/auth')
 const bcrypt = require('bcrypt')
 const express = require('express')
+const query = require('../utils/queries')
+const token = require('../utils/jwt')
 const router = express.Router()
-const config = require('config')
-const jwt = require('jsonwebtoken')
-
-router.get('/current', auth, async (req, res) => {
-  const user = await User.findById(req.user._id).select('-password')
-  res.send(user)
-})
 
 router.post('/register', async (req, res) => {
   const { name, email, password } = req.body
+  if (!name || !email || !password) {
+      res.sendStatus(418)
+      return
+  }
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const createUserMutation = `mutation createUser { insert_users(objects: {name: "${name}", password: "${hashedPassword}", email: "${email}"}) {
+        returning {
+          id
+        }
+      }}`
+    const created = await query(createUserMutation)
+    res.send(created)
 })
 
-router.post('/', async (req, res) => {
-  // validate the request body first
-
-  user = {
-    _id: '123',
-    name: 'name',
-    email: 'tes@ti.fi',
-  }
-
-  const token = jwt.sign(
-    {
-      sub: '1234567890',
-      name: 'Node App',
-      admin: true,
-      iat: 1516239022,
-      'https://hasura.io/jwt/claims': {
-        'x-hasura-allowed-roles': ['editor', 'user', 'mod'],
-        'x-hasura-default-role': 'user',
-      },
-    },
-    config.get('myprivatekey')
-  )
-
-  res.header('Authorization', 'Bearer ' + token).send({
-    _id: user._id,
-    name: user.name,
-    email: user.email,
-  })
+router.post('/login', async (req, res) => {
+    if (!req.body.user) {
+        res.send(400)
+    }
+    const findUserQuery = `query findUser { users_aggregate(where: {name: {_eq: "${req.body.user}" }} ){nodes{password}}}`
+    const queried = await query(findUserQuery)
+    const found = queried.data.users_aggregate.nodes[0].password
+    if (!req.body.password) {
+        res.send(401, "Please give password")
+    } else if (!found) {
+        res.send(401, "Username not found")
+    } else {
+        bcrypt.compare(req.body.password, found, (err, result) => {
+            if (err) throw err
+            if (result) {
+                res.send(200, {"token": token})
+            } else {
+                res.sendStatus(401)
+            }
+        })
+    }
+  
 })
 
 module.exports = router
