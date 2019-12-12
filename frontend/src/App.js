69d717e9afa27047271475ff3f62a6a41f7fb026
Lauri Nuotio <lauri.nuotio@siili.com>
2019-12-12 12:26:44 +0200

    feat(login): login w/ jwt+user on localstorage on global state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,19 +6,44 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Navbar from "react-bootstrap/Navbar";
 import Home from "./components/Home";
+import { useGlobalState, setUser } from "./stateContext";
 
 function App() {
+  const [user] = useGlobalState("user");
+
+  useEffect(() => {
+    const userFromStorage = localStorage.getItem("user");
+    if (userFromStorage) {
+      setUser(userFromStorage);
+    }
+  });
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
   return (
     <Router>
       <Container>
-        <Row>
-          <nav>
-            <Link to="/">Home</Link>
+        <Row></Row>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Sample Pipeline</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Link to="/">Home</Link>
+          {user ? (
+            <Link to="/" onClick={logout}>
+              Log out
+            </Link>
+          ) : (
             <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </nav>
-        </Row>
+          )}
+          <Link to="/register">Register</Link>
+        </Navbar>
+
         <Row>
           <Switch>
             <Route path="/login">
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,21 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { setUser } from "../stateContext";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState("");
+
+  const submitHandler = e => {
+    e.preventDefault();
+    fetch("http://localhost:3000/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({ user: username, password: password })
+    })
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        if (json.token) {
+          localStorage.setItem("token", json.token);
+          localStorage.setItem("user", { username, password });
+          setUser({ username });
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        setLoginError("Wrong username and/or password!");
+      });
+  };
+
   return (
     <div>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Email" />
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Username"
+            name="email"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
         </Form.Group>
+        {loginError ? (
+          <Form.Group controlId="formBasicError">
+            <Form.Label>{loginError}</Form.Label>
+          </Form.Group>
+        ) : (
+          <div></div>
+        )}
+
         <Button variant="primary" type="submit">
           Login
         </Button>
