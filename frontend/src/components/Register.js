c9a132f7149b466221e17892058ef1ae0e6f899b
Lauri Nuotio <lauri.nuotio@siili.com>
2019-12-13 10:38:18 +0200

    feat(register): init register functionality

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,28 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [registerError, setRegisterError] = useState(null);
+
+  const submitHandler = e => {
+    e.preventDefault();
+    fetch("http://localhost:3000/api/users/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({ username, password, email })
+    })
+      .then(res => res.json())
+      .then(json => {
+        console.log("response", json);
+        if (json.data.insert_users.returning) {
+          // success
+        } else {
+          // something went wrong
+          setRegisterError("Something went wrong");
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        setRegisterError("Something went wrong");
+      });
+  };
+
   return (
     <div>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Name" />
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="formBasicName">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Name"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
 
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Email" />
+          <Form.Control
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
         </Form.Group>
-
+        <Alert show={registerError} variant="danger">
+          {registerError}
+        </Alert>
         <Button variant="primary" type="submit">
           Register
         </Button>
