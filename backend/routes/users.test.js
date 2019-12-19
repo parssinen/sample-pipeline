7db9663b320b51983bf8f6c644e450c8819cc099
Lauri Nuotio <latezio@gmail.com>
2019-12-19 08:33:58 +0200

    fix(tests): pass done

--- backend/routes/users.test.js
+++ backend/routes/users.test.js
@@ -1,50 +1,59 @@
 const request = require('supertest')
+const assert = require('assert')
 const app = require('../index')
 
 describe('Users endpoint', () => {
-  it('should not allow registration without email', async () => {
+  it('should not allow registration without email', async done => {
     request(app)
       .post('/api/users/register')
       .send({ username: 'Testuser', password: 'Testpassword' })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
       .expect(418)
+      .end(err => {
+        if (err) return done(err)
+        done()
+      })
   })
-  it('should not allow registration without password', async () => {
+  it('should not allow registration without password', async done => {
     request(app)
       .post('/api/users/register')
       .send({ username: 'Testuser', email: 'Testpassword' })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
       .expect(418)
+      .end(err => {
+        if (err) return done(err)
+        done()
+      })
   })
-  it('should not allow registration without username', async () => {
+  it('should not allow registration without username', async done => {
     request(app)
       .post('/api/users/register')
       .send({ email: 'Testuser', password: 'Testpassword' })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
       .expect(418)
+      .end(err => {
+        if (err) return done(err)
+        done()
+      })
   })
-  it('should register ok', async () => {
+  it('should register ok', async done => {
     request(app)
       .post('/api/users/register')
       .send({
-        email: 'Testuser',
+        username: 'Testuser',
         password: 'Testpassword',
         email: 'test@email.com',
       })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
       .expect(200)
+      .end(err => {
+        if (err) return done(err)
+        done()
+      })
   })
 })
 
 describe('Login route', () => {
-  it('should return JWT', async () => {
+  it('should return JWT', async done => {
     const res = await request(app)
       .post('/api/users/login')
-      .send({ username: 'Testuser', password: 'Testpassword' })
+      .send({ user: 'Testuser', password: 'Testpassword' })
       .expect(200)
       .end(err => {
         if (err) return done(err)
