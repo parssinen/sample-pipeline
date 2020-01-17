fe1f9a5c9257db5d1aa4bd87c09c94b3c4f31f84
Lauri Nuotio <latezio@gmail.com>
2020-01-17 09:40:10 +0200

    feat(register): ensure unique username and email

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -11,6 +11,25 @@ router.post('/register', async (req, res) => {
     res.sendStatus(418)
     return
   }
+  // Ensure email uniqueness
+  const ensureUniqueEmail = `query findEmail { users_aggregate(where: {email: {_eq: "${email}" }} ){nodes{id}}}`
+  const uniqueEmail = await query(ensureUniqueEmail)
+  if (!uniqueEmail.data.users_aggregate.nodes.length < 1) {
+    console.log('ei ole', uniqueEmail.data.users_aggregate.nodes)
+    res.status(418).send({ error: 'Email already taken' })
+    return
+  }
+
+  // Ensure username uniqueness
+  const ensureUniqueUsername = `query findUser { users_aggregate(where: {name: {_eq: "${username}" }} ){nodes{id}}}`
+  const uniqueUsername = await query(ensureUniqueUsername)
+  if (!uniqueUsername.data.users_aggregate.nodes.length < 1) {
+    console.log('ei ole', uniqueEmail.data.users_aggregate.nodes)
+    res.status(418).send({ error: 'Username already taken' })
+    return
+  }
+
+  // Create user
   const hashedPassword = await bcrypt.hash(password, 10)
   const createUserMutation = `mutation createUser { insert_users(objects: {name: "${username}", password: "${hashedPassword}", email: "${email}"}) {
         returning {
